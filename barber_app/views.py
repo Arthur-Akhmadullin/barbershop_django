feafca2c778ed367e77fdaa26892a0f4ad94d815
Arthur_Akhmadullin <akhmadullinartur@yandex.ru>
2020-10-19 23:55:38 +0500

    Шаблонизация html-страниц

--- barber_app/views.py
+++ barber_app/views.py
@@ -3,3 +3,12 @@ from django.urls import reverse
 
 def main_page(request):
 	return render(request, 'barber_app/index.html')
+	
+def price_page(request):
+	return render(request, 'barber_app/price.html')
+
+def shop_page(request):
+	return render(request, 'barber_app/shop.html')
+
+def item_page(request):
+	return render(request, 'barber_app/item.html')
