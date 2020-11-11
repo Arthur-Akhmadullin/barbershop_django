6d2ff0803214233a3eb719c5125b0ea827443cd3
Arthur_Akhmadullin <akhmadullinartur@yandex.ru>
2020-11-11 17:36:43 +0500

    Добавлен класс News в models.py

--- barber_app/models.py
+++ barber_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+	title = models.CharField(max_length=250, db_index=True)
+	slug = models.SlugField(max_length=250, unique=True)
+	body = models.TextField(blank=True)
+	date = models.DateField(auto_now_add=True)
